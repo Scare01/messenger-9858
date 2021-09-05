8018058d9756c532d3c0050d08cf5acf74151ebb
Слесарев Алексей <slesarev.aleksej@fitauto.ru>
2021-09-05 12:13:02 +0700

    install cloudinary

--- client/src/components/ActiveChat/Input.js
+++ client/src/components/ActiveChat/Input.js
@@ -1,20 +1,48 @@
-import React, { useState } from "react";
-import { FormControl, FilledInput } from "@material-ui/core";
+import React, { useRef, useState } from "react";
+import { FormControl, FilledInput, SvgIcon } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { connect } from "react-redux";
 import { postMessage } from "../../store/utils/thunkCreators";
 
+import InsertDriveFileIcon from '@material-ui/icons/InsertDriveFile';
+
+const cloudinary = require('cloudinary/lib/cloudinary');
+
 const useStyles = makeStyles(() => ({
   root: {
     justifySelf: "flex-end",
-    marginTop: 15
+    marginTop: 15,
+    position: 'relative',
+
+    '& .icon-block': {
+      position: 'absolute',
+      right: '10px',
+      top: '20%',
+
+      '& > svg': {
+        fill: 'lightgray',
+
+        '&:hover': {
+          cursor: 'pointer',
+        },
+      },
+    },
+
+    '& .input-upload': {
+      // display: 'none',
+      visibility: 'hidden',
+    },
+
   },
   input: {
     height: 70,
     backgroundColor: "#F4F6FA",
     borderRadius: 8,
-    marginBottom: 20
-  }
+    marginBottom: 20,
+  },
+  inputUpload: {
+    display: 'none',
+  },
 }));
 
 const Input = (props) => {
@@ -22,9 +50,20 @@ const Input = (props) => {
   const [text, setText] = useState("");
   const { postMessage, otherUser, conversationId, user } = props;
 
+  const inputFile = useRef(null)
+
   const handleChange = (event) => {
     setText(event.target.value);
   };
+
+  const uploadFiles = (file) => {
+    cloudinary.uploader.upload(file, function(error, result) {console.log(result, error)});
+  }
+
+
+  const prepareFilesToUpload = (files) => {
+    files.forEach(file => uploadFiles(file.name));
+  }
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -49,6 +88,18 @@ const Input = (props) => {
           value={text}
           name="text"
           onChange={handleChange}
+        />
+        <div className='icon-block'>
+          <SvgIcon
+              component={InsertDriveFileIcon}
+              onClick={() => inputFile.current.click()}
+          />
+        </div>
+        <input
+            className='input-upload'
+            type="file"
+            ref={inputFile}
+            onChange={event => uploadFiles(event.target.files)}
         />
       </FormControl>
     </form>
